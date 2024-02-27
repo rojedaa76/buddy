177cc300dcb89b4e517e811a1fe3870289ab04b0
rojedaa76@gmail.com <rojedaa76@gmail.com>
2024-02-27 15:22:31 -0500

    agrego 2 filas del proyecto del api de buddy

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzODEyMw/spectral.js?branch_slug=master&token=f8c99fa6-33e5-48ab-b214-7960bb572e0a";
+export default { extends: ruleset };
